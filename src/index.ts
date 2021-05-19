b015f27944901a4c70881f82d4becbda3e6e6710
Aliaskar <aliaskar.urakov@samgau.com>
2021-05-19 16:50:26 +0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import express from 'express';
+import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
+import microConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
+
+const main = async () => {
+	const orm = await MikroORM.init(microConfig);
+
+	await orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver, UserResolver],
+			validate: false,
+		}),
+		context: () => ({ em: orm.em }),
+	});
+
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log('server started on localhost:4000');
+	});
+};
+
+main().catch(err => {
+	console.error(err);
+});
